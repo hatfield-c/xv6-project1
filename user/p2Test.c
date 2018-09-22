ae79d5dd2dabf95960fd4ae436487c1943fa9f21
Cody Robert Hatfield <cxh124730@csjaws.utdallas.edu>
2018-09-22 00:39:27 -0500

    [RED-7] Implement Official Testing Scripts

--- user/p2Test.c
+++
@@ -1,9 +0,0 @@
-#include "syscall.h"
-#include "types.h"
-#include "user.h"
-
-int main(int argc, char *argv[]){
-	int count = getreadcount();
-	printf(1, "\n[Read Count]: %d\n", count);
-	exit();;
-}
